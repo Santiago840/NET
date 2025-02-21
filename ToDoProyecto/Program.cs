d8fde7b45062a98294aa1799020293fe7d30fc85
Santiago840 <santyemma32@gmail.com>
2025-02-21 01:31:47 -0600

    ASP.NET init

---
+++ ToDoProyecto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ToDoProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
